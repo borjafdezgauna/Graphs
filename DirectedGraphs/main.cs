efc2ff0645b637d61bc09785d0d3d511ff253ca8
Borja Fernández Gauna <borjafdezgauna>
2024-09-04 13:15:17 +0200

    Removed api-related stuff

--- DirectedGraphs/main.cs
+++ DirectedGraphs/main.cs
@@ -6,32 +6,19 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        StatsUpdater statsUpdater = new StatsUpdater();
-        statsUpdater.CheckGitUsername();
-
-        if (statsUpdater.GitUsername == null)
-        {
-            Console.WriteLine("Error getting Git info. You need to add this project to Git (fork from my repo and then clone your fork)");
-            return;
-        }
-
         Console.WriteLine("Testing directed graphs...");
         if (!Tests.TestDirectedGraphs())
             return;
 
-        Console.WriteLine("\nAll tests passed. Measuring speed...");
+        Console.WriteLine("\nAll tests passed. Testing distance calculator...");
         SpeedMeasure speedMeasure = Tests.TestDistanceCalculator();
 
         if (!speedMeasure.Success)
         {
-            Console.WriteLine("An error was detected during the speed measurement. Probably something wrong with Add/Get");
+            Console.WriteLine("An error was detected testing the distance calculator");
             return;
         }
 
-        bool success = statsUpdater.AddResult(speedMeasure);
-        if (success)
-            Console.WriteLine("Your time was saved on the server");
-        else
-            Console.WriteLine("Something failed saving your time on the server");
+        Console.WriteLine($"All tests passed. Time: {speedMeasure.Time}s");
     }
 }
--- UnDirectedGraphs/main.cs
+++ UnDirectedGraphs/main.cs
@@ -5,19 +5,9 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        StatsUpdater statsUpdater = new StatsUpdater();
-        statsUpdater.CheckGitUsername();
-
-        if (statsUpdater.GitUsername == null)
-        {
-            Console.WriteLine("Error getting Git info. You need to add this project to Git (fork from my repo and then clone your fork)");
-            return;
-        }
-
         Console.WriteLine("Testing undirected graphs...");
         if (!Tests.TestUnDirectedGraphs())
             return;
 
-        statsUpdater.AddResult(null);
     }
 }
